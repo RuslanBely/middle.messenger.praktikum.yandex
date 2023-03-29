00fb92e3178e5b860f739626042c7d78cca3c3b0
Ruslan Belyi <baiso@ya.ru>
2023-03-29 23:02:33 +0300

    Delete 404 pages

--- src/pages/404/404Tpl.ts
+++
@@ -1,8 +0,0 @@
-export const  p404Tpl =`
-<div class="p404">
-      <h1 class="p404__h1">404</h1>
-      <p class="p404__descr">Не туда попали</p>
-      <a href="#" class="p404__link">Назад к чатам</a>
-</div>
-`
-;
--- src/pages/404/index.ts
+++
@@ -1,7 +0,0 @@
-import Handlebars from  "handlebars";
-import { p404Tpl } from './404Tpl'
-import "./404.scss"
-
-export const p404 = () => {
-  return Handlebars.compile(p404Tpl)({});
-};
